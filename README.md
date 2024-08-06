df2fd1570e23a33f56165a101135538e79d7c557
Rin <142712029+rindotsh@users.noreply.github.com>
2024-08-06 12:26:33 -0500

    Adding first files
    contains the installaer, the script itself and a README.md

---
+++ README.md
@@ -0,0 +1,11 @@
+
+
+
+RINDOTSH's REFLECTOR UPDATER:
+
+This is a simple bash script that will help update your mirrorlist with reflector without having to type out the entire command
+Script takes into account that you may not have reflector installed and can help you install it
+
+To download the latest version of this script:
+    git clone https://github.com/rindotsh/reflector-updater
+ps: this is my first ever actual coding project, a small but good start. Here's to many more.
---
+++ install.sh
@@ -0,0 +1,46 @@
+echo "######################################"
+echo " WELCOME TO RIN'S FIRST FOSS PROJECT"
+echo "######################################"
+
+echo "this is a reflector updating script for pacman, which can be run with the command 'reflect'"
+
+#initializes a user read-response
+read -p "would you like to install or uninstall this project from your machine? (i/u) " answer
+
+#location of apps
+reflectapp="/home/$USER/reflector-updater/reflect"
+reflectbin="/usr/bin/reflect"
+
+#installs the app, if app not found, error command is shown
+install() {
+	if [ -e "$reflectapp" ]; then
+		echo "installing reflector-updater...."
+		sudo cp $reflectapp $reflectbin
+		sudo chmod +x $reflectbin
+	else
+		echo "error: ~/reflector-updater/reflect File not found!"
+	fi
+}
+#uninstalls the app, if app is not found, error command is shown
+uninstall() {
+	if [ -e "$reflectbin" ]; then
+		echo "uninstalling reflector-updater...."
+		sudo rm $reflectbin
+	else
+		echo "error: /usr/bin/reflect Not found!"
+	fi
+}
+	
+#uses case to make sure user inputs i or u, and will not accept any other commands
+case "$answer" in
+
+	i) 
+		install 
+		;;
+	u)	uninstall
+		;;
+	*)
+		echo "Invalid response"
+		reading
+		;;
+esac
